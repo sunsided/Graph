ebe928fd00a40671948673e08bf5a193bc017b76
Markus Mayer <development@widemeadows.de>
2011-06-18 23:21:00 +0200

    Filter erzeugt

---
+++ Graph/IFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace Graph
+{
+	/// <summary>
+	/// Interface für Filter
+	/// </summary>
+	/// <typeparam name="TIn">Eingabedatentyp</typeparam>
+	/// <typeparam name="TOut">Ausgabedatentyp</typeparam>
+	public interface IFilter<in TIn, out TOut>
+	{
+		/// <summary>
+		/// Verarbeitet die Eingabe
+		/// </summary>
+		/// <param name="input">Der zu verarbeitende Wert</param>
+		/// <returns>Das Ergebnis</returns>
+		TOut Filter(TIn input);
+	}
+}
